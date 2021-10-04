7f78040cd7ebdee468b7c86783a2a200d5185ba7
Loai Alhakeam <77158117+LoaiAlhakeam@users.noreply.github.com>
2021-10-04 23:23:08 +0300

    Toggle task checkbox

--- lib/Components/task_item.dart
+++ lib/Components/task_item.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
-class TaskItem extends StatelessWidget {
+class TaskItem extends StatefulWidget {
+  @override
+  _TaskItemState createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  bool checkState = true;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        'Task',
+        style: TextStyle(
+          decoration: checkState ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckbox(
+        isCheck: checkState,
+        toggleCheckbox: (bool? newValue) {
+          setState(() {
+            checkState = newValue!;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool isCheck;
+  final Function toggleCheckbox;
+
+  TaskCheckbox({required this.isCheck, required this.toggleCheckbox});
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isCheck,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: (value) {
+        toggleCheckbox(value);
+      },
     );
   }
 }
